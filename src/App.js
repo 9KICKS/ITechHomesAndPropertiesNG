6f8adecd10911b3743c1927f0707518818380eb0
Omisakin Joshua <officialjoshua9@gmail.com>
2024-02-16 16:11:47 +0100

    "updated the app"

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import LandingPage from "./components/LandingPage";
+import ClientLoginPage from "./components/ClientLoginPage";
 
 function App() {
   return (
     <div className="App">
-      <LandingPage />
+      {/*<LandingPage />*/}
+      <ClientLoginPage />
     </div>
   );
 }
