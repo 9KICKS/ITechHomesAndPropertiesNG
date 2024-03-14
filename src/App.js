5473903b2b005a7d5f0e6487ee87a585a59d133c
Omisakin Joshua <officialjoshua9@gmail.com>
2024-03-14 18:58:34 +0100

    "latest update"

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import LandingPage from "./components/LandingPage";
 import ClientLoginPage from "./components/ClientLoginPage";
 import ForgotPassword from "./components/ForgotPassword";
 import OTPPage from "./components/OTPPage";
+import Dashboard from "./admin/dashboard/Dashboard";
+import Sidebar from "./admin/dashboard/Sidebar";
+import {useState} from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <LandingPage />
-      {/*<ClientLoginPage />*/}
-      {/*<ForgotPassword />*/}
-      {/*<OTPPage />*/}
-    </div>
-  );
+    const [activePage, setActivePage] = useState('');
+
+    return (
+        <div className="App">
+            <Sidebar setActivePage={setActivePage} />
+            <div className="content">
+                {activePage === 'dashboard' && <Dashboard />}
+            </div>
+        </div>
+    );
 }
 
 export default App;
