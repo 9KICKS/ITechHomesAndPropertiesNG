356ed72c5d75001a0d61fafe9560368c969c8e81
Omisakin Joshua <officialjoshua9@gmail.com>
2024-03-18 23:27:27 +0100

    "latest update"

--- src/AppRoute.js
+++ src/AppRoute.js
@@ -1,6 +1,8 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LandingPage from "./components/LandingPage";
+import ClientLoginPage from './components/ClientLoginPage';
+import ClientSignUpPage from "./components/ClientSignUpPage";
 
 function AppRoute() {
     return (
@@ -8,6 +10,8 @@ function AppRoute() {
             <Router>
                 <Routes>
                     <Route path="/" element={<LandingPage />} />
+                    <Route path="/signup" element={<ClientSignUpPage />} />
+                    <Route path="/login" element={<ClientLoginPage />} />
                 </Routes>
             </Router>
         </div>
