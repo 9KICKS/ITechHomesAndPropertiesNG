a9c2ce986c5be17cf39c715c63690e053f63afec
Omisakin Joshua <officialjoshua9@gmail.com>
2024-03-19 04:41:02 +0100

    "latest update"

--- src/components/ClientSignUpPage.jsx
+++ src/components/ClientSignUpPage.jsx
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import { Form } from 'react-bootstrap';
+import React, {useState} from 'react';
+import {Form} from 'react-bootstrap';
 import "../styles/ClientSignUpPage.css";
 import iTechLogo from '../assets/images/iTechLogo.svg';
 
@@ -23,38 +23,44 @@ const ClientSignUpPage = () => {
             <div className="page-content">
                 <div className="col-md-6">
                     <img src={iTechLogo} alt="ITech logo" className="img-fluid"/>
-                    <h2 className="mt-3 login-title">Sign up to create your account</h2>
+                    <h2 className="mt-3 login-title">Create your account</h2>
                     <p className="welcome-message">Welcome! Please enter your details to sign up.</p>
                     <Form onSubmit={handleSignUp}>
-                        <Form.Group controlId="formBasicFirstName" className="custom-input">
-                            <Form.Label className="label-top">First Name</Form.Label>
-                            <Form.Control type="text" placeholder="Enter first name" value={firstName}
-                                          onChange={(e) => setFirstName(e.target.value)}/>
-                        </Form.Group>
+                        <div className="form-group-container">
+                            <Form.Group controlId="formBasicFirstName" className="custom-input">
+                                <Form.Label className="label-top">First Name</Form.Label>
+                                <Form.Control type="text" placeholder="Enter first name" value={firstName}
+                                              onChange={(e) => setFirstName(e.target.value)}/>
+                            </Form.Group>
 
-                        <Form.Group controlId="formBasicLastName" className="custom-input">
-                            <Form.Label className="label-top">Last Name</Form.Label>
-                            <Form.Control type="text" placeholder="Enter last name" value={lastName}
-                                          onChange={(e) => setLastName(e.target.value)}/>
-                        </Form.Group>
+                            <Form.Group controlId="formBasicLastName" className="custom-input">
+                                <Form.Label className="label-top">Last Name</Form.Label>
+                                <Form.Control type="text" placeholder="Enter last name" value={lastName}
+                                              onChange={(e) => setLastName(e.target.value)}/>
+                            </Form.Group>
 
-                        <Form.Group controlId="formBasicEmail" className="custom-input">
-                            <Form.Label className="label-top">Email</Form.Label>
-                            <Form.Control type="email" placeholder="Enter email" value={email}
-                                          onChange={(e) => setEmail(e.target.value)}/>
-                        </Form.Group>
+                            <Form.Group controlId="formBasicEmail" className="custom-input">
+                                <Form.Label className="label-top">Email</Form.Label>
+                                <Form.Control type="email" placeholder="Enter email" value={email}
+                                              onChange={(e) => setEmail(e.target.value)}/>
+                            </Form.Group>
 
-                        <Form.Group controlId="formBasicPassword" className="custom-input">
-                            <Form.Label className="label-top">Password</Form.Label>
-                            <Form.Control type="password" placeholder="Password" value={password}
-                                          onChange={(e) => setPassword(e.target.value)}/>
-                        </Form.Group>
+                            <Form.Group controlId="formBasicPassword" className="custom-input">
+                                <Form.Label className="label-top">Password</Form.Label>
+                                <Form.Control type="password" placeholder="Password" value={password}
+                                              onChange={(e) => setPassword(e.target.value)}/>
+                            </Form.Group>
 
-                        <Form.Group controlId="formBasicConfirmPassword" className="custom-input">
-                            <Form.Label className="label-top">Confirm Password</Form.Label>
-                            <Form.Control type="password" placeholder="Confirm Password" value={confirmPassword}
-                                          onChange={(e) => setConfirmPassword(e.target.value)}/>
-                        </Form.Group>
+                            <Form.Group controlId="formBasicConfirmPassword" className="custom-input">
+                                <Form.Label className="label-top">Confirm Password</Form.Label>
+                                <Form.Control type="password" placeholder="Confirm Password" value={confirmPassword}
+                                              onChange={(e) => setConfirmPassword(e.target.value)}/>
+                            </Form.Group>
+                        </div>
+
+                        <div className="already-have-account">
+                            Already have an account? <a href="/login" className="custom-link">Sign In</a>
+                        </div>
 
                         <button type="submit" className="custom-button sign-in-button">
                             Sign Up
@@ -63,6 +69,12 @@ const ClientSignUpPage = () => {
                         <button type="button" className="custom-button google-sign-in-button">
                             Sign Up with Google
                         </button>
+
+                        <div className="agreement-text">
+                            By clicking the Sign Up button, you agree to our <br/>
+                            <a href="/terms-and-conditions" className="custom-link">Terms and Conditions</a> and
+                            <a href="/privacy-policy" className="custom-link"> Policy Privacy</a>.
+                        </div>
                     </Form>
                 </div>
             </div>
