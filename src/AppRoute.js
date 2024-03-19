2736dc70ff077798241ad54c2649641c7e4e1801
Omisakin Joshua <officialjoshua9@gmail.com>
2024-03-19 04:35:37 +0100

    "latest update"

--- src/AppRoute.js
+++ src/AppRoute.js
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LandingPage from "./components/LandingPage";
 import ClientLoginPage from './components/ClientLoginPage';
 import ClientSignUpPage from "./components/ClientSignUpPage";
+import TermsAndConditions from "./components/TermsAndConditions";
+import PrivacyPolicy from "./components/PrivacyPolicy";
 
 function AppRoute() {
     return (
@@ -11,6 +13,8 @@ function AppRoute() {
                 <Routes>
                     <Route path="/" element={<LandingPage />} />
                     <Route path="/signup" element={<ClientSignUpPage />} />
+                    <Route path="/terms-and-conditions" element={<TermsAndConditions />} />
+                    <Route path="/privacy-policy" element={<PrivacyPolicy />} />
                     <Route path="/login" element={<ClientLoginPage />} />
                 </Routes>
             </Router>
